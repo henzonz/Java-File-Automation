8c470b8c1e19df1257fb4708ba59c0d2965d3b12
henzonz <zhenzon26@gmail.com>
2023-08-16 22:44:47 -0700

    Added renaming specific file feature.

--- src/FileAutomation.java
+++ src/FileAutomation.java
@@ -1,16 +1,14 @@
 import java.io.File;
 import java.util.Scanner;
 public class FileAutomation {
+    private static String directory_pathname = "";
+    static Scanner input = new Scanner(System.in);
             public static void main(String[] args) {
-                String directory_pathname = "";
+
                 try {
                     //enter pathname
-                    Scanner input = new Scanner(System.in);
                     System.out.print("Enter pathname: ");
-
                     directory_pathname = input.nextLine();
-
-//            getFileName(directory_pathname);
                 }
                 catch (Exception e){
                     System.out.println("Invalid pathname.");
@@ -18,9 +16,10 @@ public class FileAutomation {
                 Scanner command_input = new Scanner(System.in);
                 System.out.println("What would you like to do?");
                 System.out.println("1 - Look up ALL folder/file names within the directory");
-                System.out.println("2 - Look up specific a file name within the directory");
+                System.out.println("2 - Look up a specific file name within the directory");
                 System.out.println("3 - Look up ALL file names containing a specific name within the directory");
                 System.out.println("4 - Update a specific file name within the directory");
+                System.out.println("5 - Rename a file in a directory");
                 System.out.println("0 - exit");
                 System.out.print("Enter: ");
                 int command = command_input.nextInt();
@@ -28,7 +27,9 @@ public class FileAutomation {
                 switch(command){
                     case 0:
                         return;
-                    case 1: getFileName(directory_pathname);
+                    case 1: getFileName(directory_pathname+ "/");
+                        break;
+                    case 5: renameFile(directory_pathname+"/");
                         break;
                 }
 
@@ -42,9 +43,29 @@ public class FileAutomation {
 
 
                 //for each loop to iterate through individual elements
-                for (File file : files) {
+                for (File file: files) {
                     if (file.isFile() || file.isDirectory()) {
                         System.out.println(file.getName());
+                    }
+                }
+            }
+
+            public static void renameFile(String pathname){
+                File folder = new File(pathname);
+                //utilizes listFiles() method from File class to retrieve file names in a directory
+                File[] files = folder.listFiles();
+
+
+                System.out.println("Which file would you like to rename?");
+                String file_name = input.nextLine();
+
+                System.out.println("What would you like to name the file?");
+                String newFileName = input.nextLine();
+                //for each loop to iterate through individual elements
+                for (File file : files) {
+                    if (file.getName().equals(file_name)) {
+                        File rename = new File(pathname+newFileName);
+                        file.renameTo(rename);
                     }
                 }
             }
